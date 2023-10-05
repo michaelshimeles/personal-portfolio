793835410264b877a828e94d6b5c9b7772793f0b
Michael Shimeles <michaelwasihun96@gmail.com>
2023-10-05 10:59:15 -0400

    fix image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ import { GithubIcon, LinkedinIcon } from 'lucide-react';
 const Hero = ({ }) => {
     return (
         <div className='flex items-center gap-5 sm:w-[80%] md:w-[60%] lg:w-[50%] p-2'>
-            <Image src="/lolpfp.png" width={150} height={150} alt='profile picture' className='rounded-full border-4' />
+            <Image src="/lolpfp.jpg" width={150} height={150} alt='profile picture' className='rounded-full border-4' />
             <div>
                 <h1 className="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-5xl">
                     Hi, I&apos;m Michael Shimeles
