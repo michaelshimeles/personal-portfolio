6bc2e03a619e7ddc9b4a80f998ad0cae2f3e0abc
Michael Shimeles <michaelwasihun96@gmail.com>
2023-11-04 10:18:00 -0400

    Added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,6 @@ import type { Metadata } from 'next'
 import Head from 'next/head'
 import './globals.css'
 
-// const inter = Inter({ subsets: ['latin'] })
-
-
 export const metadata: Metadata = {
   keywords: ['Michael Shimeles'],
   openGraph: {
@@ -27,21 +24,12 @@ export default function RootLayout({
     <html lang="en">
       <Head>
         <title>Michael Shimeles</title>
-        <meta
-          name="description"
-          content="The NextJS boilerplate with all the stuff you need to get your product in front of customers. From idea to production in 5 minutes."
-        />
         <meta property="og:type" content="website" />
         <meta
           property="og:description"
           content="Michael Shimeles is a Full Stack Engineer, with experience building production web applications."
         />
-        <meta
-          property="og:image"
-          content="https://shipfa.st/shareMain.png"
-        />
         <meta property="og:url" content="https://www.michaelshimeles.com/" />
-        <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:creator" content="@rasmickyy" />
         <link rel="canonical" href="https://www.michaelshimeles.com/" />
       </Head>
